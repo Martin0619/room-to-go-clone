e6261252681501133e980a0baf6537cc5941bb78
Martin <martin.hernandez.sftdev@gmail.com>
2021-09-11 05:55:29 -0400

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.style.scss";
-import HomePage from "./pages/home/home.page";
-import FurniturePage from "./pages/furniture/furniture.page";
 import Layout from "./components/layout/layout.component";
-import LivingRoomsCategoriesPage from "./pages/living-rooms-categories/living-rooms-categories.page";
-import LivingRoomSetsPage from "./pages/living-room-sets/living-room-sets.page";
+import LoadingSpinner from "./components/loading-spinner/loading-spinner.component";
+
+const HomePage = lazy(() => import("./pages/home/home.page"));
+const FurniturePage = lazy(() => import("./pages/furniture/furniture.page"));
+const LivingRoomsCategoriesPage = lazy(() =>
+  import("./pages/living-rooms-categories/living-rooms-categories.page")
+);
+const LivingRoomSetsPage = lazy(() =>
+  import("./pages/living-room-sets/living-room-sets.page")
+);
 
 const App = () => (
   <Layout>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/furniture" component={FurniturePage} />
-      <Route
-        exact
-        path="/furniture/living-rooms"
-        component={LivingRoomsCategoriesPage}
-      />
-      <Route
-        exact
-        path="/furniture/living-rooms/sets"
-        component={LivingRoomSetsPage}
-      />
-      <Route path="*" render={() => <h1>Page Not Found</h1>} />
-    </Switch>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/furniture" component={FurniturePage} />
+        <Route
+          exact
+          path="/furniture/living-rooms"
+          component={LivingRoomsCategoriesPage}
+        />
+        <Route
+          exact
+          path="/furniture/living-rooms/sets"
+          component={LivingRoomSetsPage}
+        />
+        <Route path="*" render={() => <h1>Page Not Found</h1>} />
+      </Switch>
+    </Suspense>
   </Layout>
 );
 
--- src/components/loading-spinner/loading-spinner.component.jsx
+++ src/components/loading-spinner/loading-spinner.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./loading-spinner.style.scss";
+
 const LoadingSpinner = () => (
   <div className="loading">
     <div className="spinner" />
